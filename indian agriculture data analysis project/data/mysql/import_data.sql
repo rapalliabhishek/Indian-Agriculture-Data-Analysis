3e521eca87820f3c9dffd623c0d9a40ad69e29f6
rapalliabhishek <rapalliabhishek@gmail.com>
2026-06-26 13:43:55 +0530

    Add files via upload

---
+++ indian agriculture data analysis project/data/mysql/import_data.sql
@@ -0,0 +1,13 @@
+-- Project: Agricultural Cost and Yield Analysis
+
+-- Data Source:
+-- cleaned_data.csv
+
+-- Import Method:
+-- MySQL Workbench -> Table Data Import Wizard
+
+-- Target Table:
+-- crop_data
+
+-- Notes:
+-- Dataset imported successfully from CSV into MySQL.
